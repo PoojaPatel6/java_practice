06aac09bf4cd5430d9097260d9b807a2807a0030
poojaedunet <122346906+poojaedunet@users.noreply.github.com>
2024-05-22 09:43:48 +0530

    Add files via upload

---
+++ practice1/First.java
@@ -0,0 +1,42 @@
+public class First{
+
+public static void main(String []args){
+
+// System.out.println("Hello");
+// System.out.println("Welcome");
+
+// int a = 6;
+// float b = 5;
+// System.out.println(a);
+// System.out.println(b);
+
+// byte a = 127;
+// short a1 = -3299;
+// System.out.println(a);
+// System.out.println(a1);
+
+//float
+// float a = 6;
+// float b = 6.2f;
+// System.out.println(a);
+// System.out.println(b);
+
+//char
+
+// char a = 'c';
+// char b = 'A';
+
+// System.out.println(a);
+// System.out.println(b);
+
+//boolean
+
+boolean a = true;
+boolean b = false;
+
+System.out.println(a);
+System.out.println(b);
+
+
+}
+}
