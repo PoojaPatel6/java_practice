aa1db8ce0be59796d9a69d718bdda369731d8ac1
PoojaPatel6 <122346906+PoojaPatel6@users.noreply.github.com>
2024-06-02 13:01:46 +0530

    Add files via upload

---
+++ Strings/ConcatNames.java
@@ -0,0 +1,15 @@
+public class ConcatNames {
+  
+	public static void main(String[] args) {
+    
+    // Add your first names:
+    String firstName = "Pooja";  
+    String lastName = "Patel";
+
+    // What's the full name?
+    System.out.println(firstName.concat(" " + lastName));
+    
+    
+  }
+  
+}
---
+++ Strings/Initials.java
@@ -0,0 +1,19 @@
+
+// using charAt()
+
+public class Initials {
+  
+	public static void main(String[] args) {
+    
+    // Add a first name and a last name:
+    String firstName = "Prity";  
+    String lastName = "Singh";
+    
+    // What are the initials?
+    // System.out.println(str.charAt(2));
+    System.out.println(firstName.charAt(0));
+    System.out.println(lastName.charAt(0));
+    
+  }
+  
+}
